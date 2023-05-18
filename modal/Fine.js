0aecf9f1338d7a9830eec46a9d1569360d4d1d7c
gabergabora <102705568+gabergabora@users.noreply.github.com>
2023-05-19 02:22:12 +0300

    Update Fine.js

--- modal/Fine.js
+++ modal/Fine.js
@@ -2,7 +2,7 @@ const moongoose = require("mongoose");
 const Schema = moongoose.Schema;
 
 const FineSchema = new Schema(
-  {
+  {           
     member_id: { type: Schema.Types.ObjectId },
     amount: { type: Number, default: null },
     trans_type: { type: String, default: null },
